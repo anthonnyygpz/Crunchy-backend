7395b72319b589fbd31b09047b02b7a612dc1380
HaaruuKI <antonio.gallegos.palafox@gmail.com>
2024-10-07 04:39:39 -0700

    add router firebaseAuth

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routers import aws
+from app.routers import firebaseAuth
 
 app = FastAPI()
 
 app.include_router(aws.router)
+app.include_router(firebaseAuth.router)
 
 if __name__ == "__main__":
     import uvicorn
