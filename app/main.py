cf3310ff30aadb4da75dfcec92b8982b7745c148
HaaruuKI <antonio.gallegos.palafox@gmail.com>
2024-10-11 06:53:13 -0700

    include router playlist

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.routers import aws
 from app.routers import firebaseAuth
+from app.routers import playlist
 
 app = FastAPI()
 
 app.include_router(aws.router)
 app.include_router(firebaseAuth.router)
+app.include_router(playlist.router)
 
 if __name__ == "__main__":
     import uvicorn
