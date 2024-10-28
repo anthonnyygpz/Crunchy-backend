fa89346f68916dc53ebda8d22fb5404ce799f7d9
HaaruuKI <antonio.gallegos.palafox@gmail.com>
2024-10-28 06:10:25 -0700

    new format of pydantic videos.py

--- app/schemas/videos.py
+++ app/schemas/videos.py
@@ -1,10 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
-class VideoRequest(BaseModel):
-    video_key: str
+class VideoResponse(BaseModel):
+    video_name: str = Field(
+        title="Nombre del video.",
+        description="Aqui coloca el nombre del video que se desea ver.",
+    )
 
 
-class UploadVideo(BaseModel):
-    filepath: str
-    videoName: str
+class VideoCreate(BaseModel):
+    file_path: str = Field(
+        title="Directorio de local.",
+        description="Aqui se coloca el directorio completo de donde se encuentra el video que se desea subir.",
+    )
+    video_name: str = Field(
+        title="Nombre del video.",
+        description="Aqui se coloca el nombre que se le dara al video.",
+    )
