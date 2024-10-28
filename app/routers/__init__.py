c642ce9267a97bb3c64a23377ab6ab0113f3ad5b
HaaruuKI <antonio.gallegos.palafox@gmail.com>
2024-10-28 06:06:22 -0700

    initialize of credentials firehbase

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,4 +1,21 @@
 from firebase_admin import credentials, initialize_app
+from dotenv import load_dotenv
+import os
 
-cred = credentials.Certificate("service-firebase.json")
+load_dotenv()
+
+firebase_credentials = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+}
+cred = credentials.Certificate(firebase_credentials)
 initialize_app(cred)
