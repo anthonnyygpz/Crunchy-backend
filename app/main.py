e26c78b4f4e8a9d472ed1fa86fb0f695da1c3b1f
HaaruuKI <antonio.gallegos.palafox@gmail.com>
2024-10-28 06:10:58 -0700

    news include

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
-from app.routers import videos
-from app.routers import firebaseAuth
-from app.routers import playlist
+from app.routers import (
+    videos,
+    user_email_and_password_firebase_auth,
+    playlist,
+    token_firebase_auth,
+)
+from docs.documentation import configure_docs
 
-app = FastAPI(openapi_url="/openapi.json/")
+app = FastAPI(title="Crunchy", version="1.0.0")
 
+app.include_router(user_email_and_password_firebase_auth.router)
 app.include_router(videos.router)
-app.include_router(firebaseAuth.router)
 app.include_router(playlist.router)
+app.include_router(token_firebase_auth.router)
+
+
+configure_docs(app)
+
 
 if __name__ == "__main__":
     import uvicorn
