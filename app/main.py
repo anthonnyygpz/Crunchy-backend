77a012929518016e8386e964f687ddeb59d3f2d0
HaaruuKI <antonio.gallegos.palafox@gmail.com>
2024-11-05 06:20:36 -0700

    remove user_email..

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routers import (
     videos,
-    user_email_and_password_firebase_auth,
+    user,
     playlist,
     token_firebase_auth,
 )
@@ -9,9 +9,9 @@ from docs.documentation import configure_docs
 
 app = FastAPI(title="Crunchy", version="1.0.0")
 
-app.include_router(user_email_and_password_firebase_auth.router)
+app.include_router(user.router)
 app.include_router(videos.router)
-app.include_router(playlist.router)
+# app.include_router(playlist.router)
 app.include_router(token_firebase_auth.router)
 
 
