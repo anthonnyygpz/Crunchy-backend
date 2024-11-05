0869efd87935ea481e83598b0df45567d021419c
HaaruuKI <antonio.gallegos.palafox@gmail.com>
2024-11-05 06:18:15 -0700

    create database user and model user

---
+++ app/db/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Boolean, String, DateTime
+from sqlalchemy.sql import func
+from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(String(128), primary_key=True, index=True)
+    full_name = Column(String(100), unique=True, nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    profile_picture_url = Column(String(255), nullable=True)
+    created_at = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
+    user_name = Column(String(50), unique=True, nullable=True)
+    is_active = Column(Boolean(), default=1, nullable=False)
+    # email_verified = Column(Boolean(), default=0, nullable=False)
