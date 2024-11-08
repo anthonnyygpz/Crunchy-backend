58a7e16832a89dc6302daf92da609c5529edb0c6
HaaruuKI <antonio.gallegos.palafox@gmail.com>
2024-11-08 05:47:43 -0700

    unessesary

--- app/config.py
+++ app/config.py
@@ -4,11 +4,7 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "mysql+pymysql://root:antony15@localhost/Crunchy"
-    # API_V1_STR: str = ""
     PROJECT_NAME: str = "crunchy-backend"
-
-    # class Config:
-    #     env_file = ".env"
 
 
 @lru_cache()
