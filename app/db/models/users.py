88d0823c5bff379577e30088db9050a75f8d4f1a
anthonnyygpz <anthonyygpz@gmail.com>
2024-11-19 02:58:01 -0700

    change user to users

---
+++ app/db/models/users.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Boolean, String, DateTime, Text
+from sqlalchemy.sql import func
+from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(String(128), primary_key=True, index=True)
+    email = Column(String(100), nullable=False, unique=True, index=True)
+    username = Column(String(50), unique=True, nullable=True)
+    first_name = Column(String(80), nullable=False)
+    first_last_name = Column(String(50), nullable=False)
+    second_last_name = Column(String(50), nullable=False)
+    profile_picture_url = Column(Text, nullable=True)
+    created_at = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
+    is_active = Column(Boolean(), default=1, nullable=False)
