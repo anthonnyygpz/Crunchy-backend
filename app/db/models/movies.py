67e296a0f8faa643a1075f96c80277b1e5222747
anthonnyygpz <anthonyygpz@gmail.com>
2024-11-19 02:56:50 -0700

    change structure models db

--- app/db/models/movies.py
+++ app/db/models/movies.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Boolean, Integer, String, DateTime, Numeric
+from sqlalchemy import Column, Boolean, Integer, String, DateTime, Numeric, Text
 from sqlalchemy.sql import func
 from app.db.database import Base
 
@@ -6,15 +6,17 @@ from app.db.database import Base
 class Movie(Base):
     __tablename__ = "movies"
 
-    movie_id = Column(Integer(), primary_key=True, index=True, autoincrement=True)
-    title = Column(String(100), index=True, nullable=False)
-    description = Column(String(255), nullable=True)
-    release_year = Column(Integer(), nullable=True)
+    movie_id = Column(
+        Integer(), nullable=False, primary_key=True, index=True, autoincrement=True
+    )
+    title = Column(String(100), nullable=False, unique=True, index=True)
+    description = Column(Text, nullable=True)
+    release_year = Column(Integer, nullable=True)
     duration = Column(Integer, nullable=True)
     director = Column(String(100), nullable=True)
-    thumbnail_url = Column(String(), nullable=True)
+    thumbnail_url = Column(Text, nullable=True)
     rating = Column(Numeric(3, 1), nullable=True)
     maturity_rating = Column(String(10), nullable=True)
-    created_at = Column(DateTime(timezone=True), default=func.now(), nullable=False)
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
-    is_active = Column(Boolean(), default=1, nullable=False)
+    created_at = Column(DateTime(timezone=True), nullable=False, default=func.now())
+    updated_at = Column(DateTime(timezone=True), nullable=False, onupdate=func.now())
+    is_active = Column(Boolean, nullable=False, default=1)
