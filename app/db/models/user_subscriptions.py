dd1935ae6d3eb35612236d210cc195a0745192eb
anthonnyygpz <anthonyygpz@gmail.com>
2024-11-19 02:58:18 -0700

    models user_subscriptions db

---
+++ app/db/models/user_subscriptions.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, DateTime, Integer, String, Enum, Boolean
+
+from app.db.database import Base
+
+
+class UserSubscriptions(Base):
+    subscription_id = Column(
+        Integer, nullable=False, primary_key=True, autoincrement=True
+    )
+    user_id = Column(String(128), nullable=True)
+    plan_id = Column(Integer, nullable=True)
+    start_date = Column(DateTime(timezone=True), nullable=False)
+    end_date = Column(DateTime(timezone=True), nullable=False)
+    status = Column(Enum("active", "cancelled", "expired"), nullable=True)
+    auto_renewal = Column(Boolean, nullable=False, default=True)
