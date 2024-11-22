f990f9dbde4c29dcd7e98fc4f4186e8650f1cf2d
anthonnyygpz <anthonyygpz@gmail.com>
2024-11-22 04:45:19 -0700

    database new

--- app/config.py
+++ app/config.py
@@ -3,7 +3,8 @@ from functools import lru_cache
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "mysql+pymysql://root:antony15@localhost/Crunchy"
+    # DATABASE_URL: str = "mysql+pymysql://root:antony15@localhost/Crunchy"
+    DATABASE_URL: str = "mysql+pymysql://root:dQMNCjYyTcAtVrstREBCjryfsogwkYGL@junction.proxy.rlwy.net:41691/railway"
     PROJECT_NAME: str = "crunchy-backend"
 
 
