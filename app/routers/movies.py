4637b4b1c7074a126e95fb0ec6e05a39f190c4a5
anthonnyygpz <anthonyygpz@gmail.com>
2024-11-27 00:25:09 -0700

    remove is_admin

--- app/routers/movies.py
+++ app/routers/movies.py
@@ -4,13 +4,17 @@ from sqlalchemy.orm import Session
 from app.dependencies import get_db
 from app.services.movies import MovieServiceDB
 from app.schemas.movies import MovieUploadData, MovieResponse
-from app.utils.verify_token.verify_token import is_admin
+from app.utils.verify_token.verify_token import get_current_user, is_admin
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/api",
+    tags=["movies-admin"],
+    responses={404: {"description": "Not found"}},
+)
 
 
 @router.post(
-    "/api/uploads",
+    "/uploads",
     # response_model=Mov,
     summary="Subir videos.",
     description="Almacena los videos para mostrar en la aplicacion web.",
@@ -26,23 +30,22 @@ async def uploads(
 
 
 @router.get(
-    "/api/get_videos_name",
+    "/get_videos_name",
     # response_model=dict,
     summary="Listar los videos.",
     description="Lista todo los video que hay almacenados.",
-    tags=["movies-admin"],
 )
 async def get_videos_name(
     skip: int = 1,
     limit: int = 100,
-    token: dict = Depends(is_admin),
+    token: dict = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
     return MovieServiceDB(db).get_videos_name(skip, limit)
 
 
 @router.get(
-    "/api/generate_urls/{video_name}",
+    "/generate_urls/{video_name}",
     response_model=MovieResponse,
     summary="Obtener la url.",
     description="Genera la url que mostrara la url del video.",
@@ -55,12 +58,12 @@ async def generate_urls(
 
 
 @router.get(
-    "/api/details_movie",
+    "/details_movie",
     summary="Detalles de las peliculas",
     description="Obtiene los detalles de las peliculas.",
     tags=["movies-admin"],
 )
 async def details_movie(
-    title: str, token: dict = Depends(is_admin), db: Session = Depends(get_db)
+    title: str, token: dict = Depends(get_current_user), db: Session = Depends(get_db)
 ):
     return MovieServiceDB(db).details_movie(title)
